3e1f36b08e8aa90a22fa7a7b40be87e7f523ea4b
tsahiBarshevsky <tsahi.13@gmail.com>
2021-05-14 14:08:34 +0300

    About section

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import ReceiptRoundedIcon from '@material-ui/icons/ReceiptRounded';
-// import PhoneAndroidRoundedIcon from '@material-ui/icons/PhoneAndroidRounded';
-// import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
-import { Button, Grid } from '@material-ui/core';
+import { Button, Grid, makeStyles } from '@material-ui/core';
+import vector1 from '../../Images/Card.svg';
+import vector2 from '../../Images/Digital.svg';
+import vector3 from '../../Images/Find your way.svg';
 import image1 from '../../Images/protect.png';
 import image2 from '../../Images/business-card.png';
 import image3 from '../../Images/socials.png';
 
+const useStyles = makeStyles(theme => ({
+	grid:
+	{
+		marginBottom: 70,
+        [theme.breakpoints.down("sm")]:
+		{
+			paddingTop: 40
+		}
+	},
+	item1: {order: 1, display: 'flex', flexDirection: 'column', alignItems: 'center'},
+	item2: {order: 2},
+	item3:
+	{
+		order: 3,
+		[theme.breakpoints.down("sm")]:
+		{
+			order: 4
+		}
+	},
+	item4:
+	{
+		display: 'flex', flexDirection: 'column', alignItems: 'center',
+		order: 4,
+		[theme.breakpoints.down("sm")]:
+		{
+			order: 3
+		}
+	},
+	item5: {order: 5, display: 'flex', flexDirection: 'column', alignItems: 'center'},
+	item6: {order: 6}
+}));
+
 export default function Homepage()
 {
+    const classes = useStyles();
     const account = {
         backgroundColor: '#ffce00',
         backgroundImage: "url(" + image1 + ")",
@@ -41,76 +74,51 @@ export default function Homepage()
                     component={Link} 
                     to='/registartion'>Get started</Button>
             </div>
-            {/* <div className="about-container">
-                <Grid container direction="row" justify="center" alignItems="center">
-                    <Grid item>
-                        <div className="about">
-                            <div className="icon-container">
-                                <ReceiptRoundedIcon className="icon" />
-                            </div>
-                            <h2 className="title">Saves paper</h2>
+            <div className="about-container">
+                <Grid
+                    className={classes.grid}
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    alignContent="center">
+                        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} className={classes.item1}>
+                            <h1 className="title">It's one business card</h1>
                             <p className="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus pellentesque quam ac viverra. Curabitur.
+                                But the possibilities are endless! Forget about the small and traditional business
+                                card and create a digital card with rich content, photos, videos, links to social 
+                                networks, a map and the option to navigate the business.
                             </p>
-                        </div>
-                    </Grid>
-                    <Grid item>
-                        <div className="about">
-                            <div className="icon-container">
-                                <PhoneAndroidRoundedIcon className="icon" />
-                            </div>
-                            <h2 className="title">Portable</h2>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} className={classes.item2}>
+                            <img src={vector1} alt="Vector1" />
+                        </Grid>
+                        {/*---*/}
+                        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} className={classes.item3}>
+                            <img src={vector2} alt="Vector2" />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} className={classes.item4}>
+                            <h1 className="title">We're living in a digital world</h1>
                             <p className="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus pellentesque quam ac viverra. Curabitur.
+                                So accordingly and for the sake of preserving the planet and the environment, 
+                                there is no point in spending money and resources on paper and printing. 
+                                It's better to create one card and spread it to anyone you want using one link.
                             </p>
-                        </div>
-                    </Grid>
-                    <Grid item>
-                        <div className="about">
-                            <div className="icon-container">
-                                <AccountCircleRoundedIcon className="icon" />
-                            </div>
-                            <h2 className="title">One account</h2>
+                        </Grid>
+                        {/*---*/}
+                        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} className={classes.item5}>
+                            <h1 className="title">Earn full mobility</h1>
                             <p className="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus pellentesque quam ac viverra. Curabitur.
+                                And don't worry about wasting money on having enough "hard" copies. 
+                                Your customers are on your cell phone anyway, so use it to let them 
+                                find you reach you by call, message or navigation in a short and convenient way.
                             </p>
-                        </div>
-                    </Grid>
-                    <Grid item>
-                        <div className="about">
-                            <div className="icon-container">
-                                <AcUnitRoundedIcon className="icon" />
-                            </div>
-                            <h2 className="title">Lorem ipsum</h2>
-                            <p className="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus pellentesque quam ac viverra. Curabitur.
-                            </p>
-                        </div>
-                    </Grid>
-                    <Grid item>
-                        <div className="about">
-                            <div className="icon-container">
-                                <AcUnitRoundedIcon className="icon" />
-                            </div>
-                            <h2 className="title">Lorem ipsum</h2>
-                            <p className="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus pellentesque quam ac viverra. Curabitur.
-                            </p>
-                        </div>
-                    </Grid>
-                    <Grid item>
-                        <div className="about">
-                            <div className="icon-container">
-                                <AcUnitRoundedIcon className="icon" />
-                            </div>
-                            <h2 className="title">Lorem ipsum</h2>
-                            <p className="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus pellentesque quam ac viverra. Curabitur.
-                            </p>
-                        </div>
-                    </Grid>
-                </Grid>
-            </div> */}
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} className={classes.item6}>
+                            <img src={vector3} alt="Vector3" />
+                        </Grid>
+                </Grid> 
+            </div>
             <div className="how-it-works-container">
                 <h1>How does it work?</h1>
                 <Grid container direction="row" justify="center" alignItems="center">
